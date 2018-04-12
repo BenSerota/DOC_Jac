334a8ef285154a36f2ddb29eea17c989220b81ba
BenSerota <serota@post.bgu.ac.il>
2018-04-12 14:16:11 +0300

    .

--- DOC_basic.m
+++ DOC_basic.m
@@ -5,15 +5,16 @@ Fucked_up_stuff = {};
 c = 0;
 
 if ispc
-    in_root = 'E:\DOC\Data from Jaco\';
-    out_root = 'E:\DOC\Data_Jaco_Prep\';
+    in_root = 'E:\Ben DOC\Data from Jaco\';
+    out_root = 'E:\Ben DOC\Data_Jaco_prep\';
     for i = length(conds):-1:1
         data_paths{i} = sprintf('%s%s',in_root,conds{i});
         out_paths{i} = sprintf('%s%s',out_root,conds{i});
     end
-    WS_path = 'E:\Dropbox\Ben Serota\momentary\WS';
-    mom_Fig_path = 'E:\Dropbox\Ben Serota\momentary\Figs';
-    avlnch_rslts = 'E:\Dropbox\Ben Serota\eeg ANALYSES\results\avalanches';
+    WS_path = 'C:\Users\BenSerota\Dropbox\Ben Serota\momentary\WS';
+    mom_Fig_path = 'C:\Users\BenSerota\Dropbox\Ben Serota\momentary\Figs';
+    LZ_rslts = 'C:\Users\BenSerota\Dropbox\Ben Serota\eeg ANALYSES\results\LZC';
+    avlnch_rslts = 'C:\Users\BenSerota\Dropbox\Ben Serota\eeg ANALYSES\results\avalanches';
     
 elseif ismac
     in_root = '/Users/admin/Desktop/DACOBO_h/origin/';
@@ -23,6 +24,7 @@ elseif ismac
         out_paths{i} = sprintf('%s%s',out_root,conds{i});
     end
         WS_path = '/Users/admin/Dropbox/Ben Serota/momentary';
+        LZ_rslts = '/Users/admin/Dropbox/Ben Serota/eeg ANALYSES/results/LZC';
         avlnch_rslts = '/Users/admin/Dropbox/Ben Serota/eeg ANALYSES/results/avalanches';
 end
 
